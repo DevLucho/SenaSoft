eeaecb0f2a7afb3014b39cbf3343cb6b4a01d8b0
Sebatja <jsparra943@misena.edu.co>
2020-10-14 11:20:24 -0500

    Se colocaron controladores y entidades

---
+++ app/Http/Controllers/SucursalController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SucursalController extends Controller
+{
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function index(){
+
+    }
+}
