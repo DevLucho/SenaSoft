c05a8c3ac688ec5d689784646c56bf2d56e28bc2
KesGroom <kesgroom@gmail.com>
2020-10-14 15:48:10 -0500

    Se realizan cambios a la interfaz principal del sistema

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\user\Desktop\hackathon\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\user\Desktop\hackathon\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\SenaSoft\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\SenaSoft\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/layouts/navUser.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{-- <img class="logo" src="{{asset('storage/icon.png')}}" alt=""> --}}
+                {{ Auth::user()->name }}
+            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                    <li class="nav-item dropdown">
+                         <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                Cerrar sesión
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</nav>
